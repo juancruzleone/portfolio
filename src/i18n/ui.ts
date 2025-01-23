186992d75d4d1090fc98e891f1faca58ba616b6b
Juan cruz Leone <juancruzleone@outlook.com>
2025-01-23 18:31:54 -0300

    Feat: Add i18n to translate to spanish/english

---
+++ src/i18n/ui.ts
@@ -0,0 +1,78 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+}
+
+export const defaultLang = "en"
+
+export const ui = {
+  en: {
+    home: {
+      name: "Juan Cruz Leone",
+      role: "Fullstack developer",
+    },
+    contact: {
+      title: "Contact",
+      email: "Email",
+      message: "Message",
+      textareaMessage: "Write your message",
+      inputEmail: "Write your email",
+      button: "Send message",
+    },
+    skills: {
+      title: "Skills",
+    },
+    aboutme: {
+      title: "About me",
+      description: "",
+    },
+    projects: {
+      title: "Projects",
+      titleInelar: "Inelar | Redesign of web",
+      description:
+        "To approve my thesis, redesign the inelar website and optimize the services provided through an admin panel",
+    },
+    nav: {
+      home: "Home",
+      aboutme: "About me",
+      skills: "Skills",
+      projects: "Projects",
+      contact: "Contact",
+    },
+  },
+  es: {
+    home: {
+      name: "Juan Cruz Leone",
+      role: "Desarrollador Fullstack",
+    },
+    contact: {
+      title: "Contacto",
+      email: "Email",
+      message: "Mensaje",
+      textareaMessage: "Escribe tu mensaje",
+      inputEmail: "Escribe tu email",
+      button: "Enviar mensaje",
+    },
+    skills: {
+      title: "Habilidades",
+    },
+    aboutme: {
+      title: "Sobre mi",
+      description: "",
+    },
+    projects: {
+      title: "Proyectos",
+      titleInelar: "Inelar | Rediseño de la web",
+      description:
+        "Para probar mi tesis, rediseñe el sitio web de inelar y optimizé los servicios prestados a través de un panel de administración",
+    },
+    nav: {
+      home: "Inicio",
+      aboutme: "Sobre mi",
+      skills: "Habilidades",
+      projects: "Proyectos",
+      contact: "Contacto",
+    },
+  },
+} as const
+
