93f733d208af533fb84b79bd53ec5d009a5b296c
Juan cruz Leone <juancruzleone@outlook.com>
2025-01-27 23:36:25 -0300

    Feat: Fix error in modal

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.mjs
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
-				montserrat: ['Monterrat', 'sans-serif']
+				montserrat: ['Montserrat', 'sans-serif']
 			},
 			colors: {
 				'principal': '#121212',
+				'principal-light': '#ffffff',
 			},
 			borderColor: {
 				'contenedor': '#d9d9d9',
@@ -16,5 +18,4 @@ export default {
 		},
 	},
 	plugins: [],
-}
-
+};
