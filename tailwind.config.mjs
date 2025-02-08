791140d9db2426a77fcc0499695d47df62702b2f
Juan Cruz Leone <jleone@DESKTOP-1LSM9HD.>
2025-02-08 02:19:29 -0300

    Feat: Modify the design in the light theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,22 @@
 // tailwind.config.mjs
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				montserrat: ['Montserrat', 'sans-serif']
-			},
-			colors: {
-				'principal': '#121212',
-				'principal-light': '#ffffff',
-			},
-			borderColor: {
-				'contenedor': '#d9d9d9',
-				'celeste': '#00FFF6',
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        principal: "#121212",
+        "principal-light": "#d9d9d9",
+      },
+      borderColor: {
+        contenedor: "#d9d9d9",
+        celeste: "#00FFF6",
+        light: "#003366",
+      },
+    },
+  },
+  plugins: [],
 };
