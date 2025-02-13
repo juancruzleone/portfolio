cf7a10303c26b3e0dbaba25293b65bfd9da3cc7b
Juan cruz Leone <juancruzleone@outlook.com>
2025-02-13 08:06:59 -0300

    Fix:  solve Cloudflare adapter warning and defineConfig error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: "static",
+  adapter: cloudflare(),
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/squoosh"
+    }
+  }
 });
